98a19ca8a622b9778ce82d2429d238e09a7d7faf
Pablo-Wynistorf <pablo.wynistorf@swisscom.com>
2025-01-15 10:03:53 +0100

    changed copy logic

--- src/public/invite/js/script.js
+++ src/public/invite/js/script.js
@@ -1,18 +1,23 @@
-function getInvitationLink() {
+let link = "";
+document.addEventListener("DOMContentLoaded", () => {
   fetch("/invite-token", {
     method: "POST",
     credentials: "include",
   })
     .then((response) => response.json())
     .then((data) => {
-      const link = `${location.origin}/login?token=${data.token}`;
-      navigator.clipboard.writeText(link);
-      return new Noty({
-        type: "success",
-        layout: "topLeft",
-        theme: "metroui",
-        text: "Link copied to clipboard!",
-        timeout: 2000,
-      }).show();
+      link = `${location.origin}/login?token=${data.token}`;
     });
+});
+
+
+function copyInvitationLink() {
+  navigator.clipboard.writeText(link);
+  return new Noty({
+    type: "success",
+    layout: "topLeft",
+    theme: "metroui",
+    text: "Link copied to clipboard!",
+    timeout: 2000,
+  }).show();
 }
